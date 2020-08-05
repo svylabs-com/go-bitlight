59fe2cc6c5efcc2d932a8d1eee2f51ca69695f02
Sridhar <sridhar@svylabs.com>
2020-08-05 07:14:55 +0200

    Adding some message structures

--- protocol/types.go
+++ protocol/types.go
@@ -1,5 +1,10 @@
 package protocol
 
+const magic = "0xD9B4BEF9"
+
+type VarInt struct {
+}
+
 // Structure of the Bitcoin Block Header
 type BlockHeader struct {
 	version           int32
@@ -8,4 +13,28 @@ type BlockHeader struct {
 	timestamp         uint32
 	bits              uint32
 	nonce             uint32
+	transactionCount  VarInt
+}
+
+type Headers struct {
+	count   VarInt
+	headers []BlockHeader
+}
+
+type Message struct {
+	magic        uint32
+	command      [12]byte
+	paylodLength uint32
+	checksum     uint32
+	payload      []byte
+}
+
+type VersionMessage struct {
+}
+
+type GetHeadersMessage struct {
+	version          uint32
+	hashCount        VarInt
+	blockLocatorHash [32]byte
+	hashStop         [32]byte
 }
